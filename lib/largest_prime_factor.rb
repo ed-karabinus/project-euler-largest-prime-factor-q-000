5057a3775a1a183875aafb2013877400b981b373
ed-karabinus <ejkarabin@gmail.com>
2015-11-19 19:32:36 -0600

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,28 @@
 # Enter your procedural solution here!
+def largest_prime_factor(number)
+  index = 2
+  prime_factor = 0
+  until index > Math.sqrt(number)
+    if number % index == 0 && is_prime?(index)
+      prime_factor = index
+    end
+    index += 1
+  end
+  prime_factor
+end
+
+def is_prime?(number)
+  if number == 2 || number % 6 == 1 || number % 6 == 5
+    index = 2
+    prime = true
+    until index >= number - 1
+      if number % index == 0
+        prime = false
+      end
+      index += 1
+    end
+    prime
+  else
+    nil
+  end
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,34 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  def initialize(parameter)
+    @parameter = parameter
+  end
+
+  def number
+    index = 2
+    prime_factor = 0
+    until index > Math.sqrt(@parameter)      
+      if @parameter % index == 0 && is_prime?(index)
+          prime_factor = index
+      end
+      index += 1
+    end
+    prime_factor
+  end
+
+  def is_prime?(number)
+    if number == 2 || number % 6 == 1 || number % 6 == 5
+      index = 2
+      prime = true
+      until index >= number - 1
+        if number % index == 0
+          prime = false
+        end
+        index += 1
+      end
+      prime
+    else
+      nil
+    end
+  end
+end
